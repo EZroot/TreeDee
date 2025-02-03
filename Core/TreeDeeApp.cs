c1155baf871727edaf79d35822dce43b02112bfd
EZRoot <utohaha@gmail.com>
2025-02-03 19:37:53 -0330

    showing different primitives, and a .obj model

--- Core/TreeDeeApp.cs
+++ Core/TreeDeeApp.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using OpenTK.Graphics.ES11;
 using OpenTK.Mathematics;
 using SDL2Engine.Core;
 using SDL2Engine.Core.Addressables.Interfaces;
@@ -18,7 +19,7 @@ public class TreeDeeApp : IGame
     private ICameraService m_cameraService;
 
     private nint m_texture;
-    private OpenGLHandle _handle;
+    private List<OpenGLHandle> m_assetHandles = new();
     public void Initialize(IServiceProvider serviceProvider)
     {
         m_renderService = serviceProvider.GetService<IRenderService>() ?? throw new NullReferenceException(nameof(IRenderService));
@@ -30,10 +31,30 @@ public class TreeDeeApp : IGame
         var tex = m_imageService.LoadTexture(m_renderService.RenderPtr, TreeDeeHelper.RESOURCES_FOLDER + "/texture.jpg");
         m_texture = tex.Texture;
         
-        _handle = m_modelService.Load3DModel(TreeDeeHelper.RESOURCES_FOLDER + "/3d/cat.obj", 
+        var model = m_modelService.Load3DModel(TreeDeeHelper.RESOURCES_FOLDER + "/3d/cat.obj", 
             TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.vert", 
             TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.frag",
             1f);
+        
+        var cube = m_modelService.LoadCube(
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.vert", 
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.frag",
+            1f);
+        
+        var quad = m_modelService.LoadQuad(
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.vert", 
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.frag",
+            1f);
+        
+        var sphere = m_modelService.LoadSphere(
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.vert", 
+            TreeDeeHelper.RESOURCES_FOLDER + "/shaders/3d/3d.frag",
+            1f);
+        
+        m_assetHandles.Add(model);
+        m_assetHandles.Add(cube);
+        m_assetHandles.Add(quad);
+        m_assetHandles.Add(sphere);
     }
     public void Update(float deltaTime)
     {
@@ -42,15 +63,24 @@ public class TreeDeeApp : IGame
 
     public void Render()
     {
-        var pos = new Vector3(0, -40, 100);
-        var modelPos = MathHelper.GetMatrixTranslation(pos);
-        var modelRotate = MathHelper.GetMatrixRotationAroundPivot(270f, 0, (float)Time.TotalTime * 100f, -pos);// * MathHelper.RotateZ((float)Time.TotalTime * 500f * Time.DeltaTime);
-        var modelMatrix = modelPos * modelRotate;
-        m_modelService.DrawModelGL(_handle, modelMatrix, (CameraGL3D)m_cameraService.GetActiveCamera(), m_texture);
-        m_imageService.DrawCubeGL(m_renderService.OpenGLHandle3D, 
-            MathHelper.GetMatrixTranslation(new Vector3(0,2,5)),
-            (CameraGL3D)m_cameraService.GetActiveCamera(),
-            m_texture);
+        for (var i = 0; i < m_assetHandles.Count; i++)
+        {
+            var z = 15;
+            var y = 3;
+            var xSpacer = 2.5f;
+            if (i == 0) 
+            { 
+                z = 100;
+                y = -40; 
+            }
+            var pos = new Vector3(0 + (i*xSpacer), y, z);
+            var modelPos = MathHelper.GetMatrixTranslation(pos);
+            var modelRotate = MathHelper.GetMatrixRotationAroundPivot(270f, 0, (float)Time.TotalTime * 100f, -pos);// * MathHelper.RotateZ((float)Time.TotalTime * 500f * Time.DeltaTime);
+            var modelMatrix = modelPos * modelRotate;
+            
+            var asset = m_assetHandles[i];
+            m_modelService.DrawModelGL(asset, modelMatrix, (CameraGL3D)m_cameraService.GetActiveCamera(), m_texture);
+        }
     }
 
     public void RenderGui()
